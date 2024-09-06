58416b6aa52c6ec6c2abb6d18dd0e424f67d2d35
Yuanhua Huang <huangyh094@gmail.com>
2024-09-06 16:58:39 +0800

    init rRNA pipeline

---
+++ huenRNA/analysis/_cmd_rRNA_align.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+## make reference for STAR (default)
+# STAR --runThreadN 3 --runMode genomeGenerate \
+#     --genomeDir starRef --genomeFastaFiles rRNA_5S_45S.fa \
+#     --sjdbGTFfile rRNA_anno_5S_45S.gtf \
+#     --sjdbOverhang 100 --genomeSAindexNbases 5
+
+
+## align reads to rRNA sequences
+STARbin=/ssd/users/yuanhua/envs/TFProb/bin/STAR
+starRef=/ssd/users/yuanhua/huenRNA/rRNAanno/starRef
+DATA_DIR=/ssd/users/yuanhua/huenRNA
+
+mkdir $DATA_DIR/rRNAbam
+
+for samp in Ctrl_Mock_1 Ctrl_Mock_2 Ctrl_Mock_3 Ctrl_cx_13 Ctrl_cx_15 Ctrl_cx_16 \
+Ctrl_iso_9 Ctrl_iso_10 Ctrl_iso_12 siZC3H8_1_18 siZC3H8_1_19 siZC3H8_1_21 \
+siZC3H8_3_25 siZC3H8_3_26 siZC3H8_3_27 siZC3H8_4_30 siZC3H8_4_31 siZC3H8_4_32
+do
+    echo $samp
+    
+    #Make out folder
+    mkdir $DATA_DIR/rRNAbam/$samp
+    cd $DATA_DIR/rRNAbam/$samp
+    
+    #Alignment (#SortedByCoordinate)
+    fastqDIR=$DATA_DIR/01.RawData/$samp
+    $STARbin --runThreadN 30 --genomeDir $starRef \
+        --readFilesIn $fastqDIR/"$samp"_1.fq.gz $fastqDIR/"$samp"_2.fq.gz \
+        --readFilesCommand zcat --outSAMtype BAM Unsorted --quantMode GeneCounts \
+        --outFilterScoreMinOverLread 0.33 --outFilterMatchNminOverLread 0.33 \
+        --outReadsUnmapped Fastx
+    
+    # #Sort
+    # samtools sort -@ 10 Aligned.out.bam -o Aligned.sortedByCoord.out.bam
+    # samtools index Aligned.sortedByCoord.out.bam
+done
---
+++ rRNAanno/readme.md
@@ -0,0 +1,9 @@
+# Annotation of human rRNA sequences
+
+## Source
+
+- RNA45SN1 (fasta downloaded from the track to include paddings)
+https://www.ncbi.nlm.nih.gov/gene/106631777
+
+- RNA5S1 (fasta downloaded from the track to include paddings; zoom out twice)
+https://www.ncbi.nlm.nih.gov/gene/100169751
